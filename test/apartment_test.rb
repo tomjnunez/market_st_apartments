2a1c5cba74a4fbcf5b5f2159db0d6051bf583707
Tom Nunez <tomjnunez@gmail.com>
2018-07-10 08:41:08 -0600

    adds functionality to iteration one

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,15 @@ require 'minitest/autorun'
 require './lib/apartment'
 
 class ApartmentTest < Minitest::Test
+  def test_it_has_a_attributes
+    a1 = Apartment.new({number: 1, monthly_rent: 800, bathrooms: 1, bedrooms: 1})
+
+    assert_equal 1, a1.number
+    assert_equal 800, a1.monthly_rent
+    assert_equal 1, a1.bathrooms
+    assert_equal 1, a1.bedrooms
+  end
+
+  
+
 end
